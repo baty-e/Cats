c88166a34cf558bdb60bf2f8830a80275fd87569
Batya <ychenikefimov@gmail.com>
2024-11-26 11:25:55 +0300

    функция для получения изображений создана

--- Cataas.py
+++ Cataas.py
@@ -7,9 +7,14 @@ from io import BytesIO
 def load_image(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()
-
-
+        response.raise_for_status() # эта строчка необходима для обработки исключений
+        image_data = BytesIO(response.content) # Читаем байты из ответа в объект BytesIO
+                                            # кладём сюда и открываем изображение с помощью PIL
+        img = Image.open(image_data) # Открываем изображение с помощью PIL
+        return ImageTk.PhotoImage(img) # если всё соответствует вернуть картинку (фото)
+        except Exception as e:
+        print(f"Ошибка при загрузке изображения: {e}")
+        return None # если найдёт ошибу ничего не возвращать
 
 
 window = Tk()
