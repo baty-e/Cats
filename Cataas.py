5cc7e7242ce3b3928c6e1b3ab60cafda191d2d1a
Batya <ychenikefimov@gmail.com>
2024-11-26 11:48:45 +0300

    Коты прилетели. Путём опробации отрегулировал окно по размеру и расположению на экране

--- Cataas.py
+++ Cataas.py
@@ -12,14 +12,19 @@ def load_image(url):
                                             # кладём сюда и открываем изображение с помощью PIL
         img = Image.open(image_data) # Открываем изображение с помощью PIL
         return ImageTk.PhotoImage(img) # если всё соответствует вернуть картинку (фото)
-        except Exception as e:
+    except Exception as e:
         print(f"Ошибка при загрузке изображения: {e}")
         return None # если найдёт ошибу ничего не возвращать
 
 
 window = Tk()
 window.title("Cats!")
-window.geometry("600x480")
+
+w = window.winfo_screenwidth()
+h = window.winfo_screenheight()
+w2 = w//2 - 400
+h2 = h//2 - 300
+window.geometry(f"800x600+{w2}+{h2}")
 
 label = Label()
 label.pack()
